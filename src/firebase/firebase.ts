c9ad730fdf607c7981313210c99c1fecae00c168
Jason Wu <wu80jason8@gmail.com>
2024-11-14 18:17:48 -0800

    fix

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDJc8V7rJIYAuPIPSd5Bh2m5hecVYwI0dA",
-  authDomain: "auth-next-js-b9097.firebaseapp.com",
-  projectId: "auth-next-js-b9097",
-  storageBucket: "auth-next-js-b9097.firebasestorage.app",
-  messagingSenderId: "966176168134",
-  appId: "1:966176168134:web:d388a04193f450fb0fdfc7",
-  measurementId: "G-LHE6TMYT6W",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
